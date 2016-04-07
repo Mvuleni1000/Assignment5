4cafd76ee0b548f94464f7144cd3d41a32913a52
Sbusiso <ehatever@put.ac.za>
2016-04-07 16:51:19 +0200

    second22 Commit

--- src/main/java/Assignment5/Domain/CarPart1.java
+++ src/main/java/Assignment5/Domain/CarPart1.java
@@ -3,7 +3,18 @@ package Assignment5.Domain;
 /**
  * Created by student on 2016/04/07.
  */
-public class CarPart1 {
+public class CarPart1 extends Parts{
 
-    public abstract String CarModelType(String Model);
+
+    public  String CarModelType(String part)
+    {
+        if(part == "1")
+            return "BMW";
+        else if(part != null)
+            return partSold.CarModelType(part);
+        else
+            return "No car for that part";
+
+
+    }
 }
